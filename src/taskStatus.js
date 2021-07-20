a24dfca452a4a30da13dfd7dc13893f809ed1182
Hubert MUGABO <mugberto@gmail.com>
2021-07-20 10:50:56 +0200

    Empty branch

--- src/taskStatus.js
+++
@@ -1,9 +0,0 @@
-export default function taskStatus(ev, index, list) {
-  list = list.map((task) => {
-    if (task.index === index) {
-      task.completed = ev.target.checked;
-    }
-    return task;
-  });
-  localStorage.setItem('tasks', JSON.stringify(list));
-}
